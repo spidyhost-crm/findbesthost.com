fb33926f75a0de9946ea960e2ef3b2eed0c14364
spidyhost-crm <vahid.shaik@worldhost.group>
2025-01-12 19:51:52 +0530

    Initial commit

---
+++ app/(website)/layout.tsx
@@ -0,0 +1,88 @@
+import "@/styles/tailwind.css";
+import { Providers } from "./providers";
+import { cx } from "@/utils/all";
+import { Inter, Lora } from "next/font/google";
+import { getSettings } from "@/lib/sanity/client";
+import Footer from "@/components/footer";
+import GetNavbar from "@/components/getnavbar";
+import { urlForImage } from "@/lib/sanity/image";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter"
+});
+
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora"
+});
+
+async function sharedMetaData(params: any) {
+  const settings = await getSettings();
+
+  return {
+    // enable this for resolving opengraph images
+    // metadataBase: new URL(settings.url),
+    title: {
+      default:
+        settings?.title ||
+        "Stablo Pro - Blog Template for Next.js & Sanity CMS",
+      template: "%s | Stablo"
+    },
+    description:
+      settings?.description ||
+      "Pro version of Stablo, popular open-source next.js and sanity blog template",
+    keywords: ["Next.js", "Sanity", "Tailwind CSS"],
+    authors: [{ name: "Surjith" }],
+    canonical: settings?.url,
+    openGraph: {
+      images: [
+        {
+          url:
+            urlForImage(settings?.openGraphImage)?.src ||
+            "/img/opengraph.jpg",
+          width: 800,
+          height: 600
+        }
+      ]
+    },
+    twitter: {
+      title: settings?.title || "Stablo Template",
+      card: "summary_large_image"
+    },
+    robots: {
+      index: true,
+      follow: true
+    }
+  };
+}
+
+export async function generateMetadata({ params }: any) {
+  return await sharedMetaData(params);
+}
+
+export default async function Layout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: any;
+}) {
+  const settings = await getSettings();
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cx(inter.variable, lora.variable)}>
+      <body className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
+        <Providers>
+          <GetNavbar {...settings} />
+          <div>{children}</div>
+          <Footer {...settings} />
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
+export const revalidate = 86400;
